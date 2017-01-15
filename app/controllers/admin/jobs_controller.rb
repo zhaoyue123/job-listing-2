e47ced6dbde127bbf3df7c8bf059498ca3ea3d7a
zhaoyue <dourayme321@163.com>
2017-01-15 10:16:31 +0800

    implement part4

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -51,6 +51,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:jobs).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email)
+    params.require(:jobs).permit(:title, :description, :wage_lower_bound, :wage_upper_bound, :contact_email, :is_hidden)
   end
 end
